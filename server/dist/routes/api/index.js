185c7f7be0038ce76b0aa8284c1724735a2028ea
DeenTradesCode <saifshihadeh1229@gmail.com>
2025-04-10 21:22:21 -0400

    frixes1

--- server/dist/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { ticketRouter } from './ticket-routes.js';
-import { userRouter } from './user-routes.js';
-import authRouter from './auth-routes.js';
-const router = Router();
-router.use('/tickets', ticketRouter);
-router.use('/users', userRouter);
-router.use('/auth', authRouter);
-export default router;
--- server/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import authRoutes from './api/auth-routes.js';
-import apiRoutes from './api/index.js';
-const router = Router();
-router.use('/auth', authRoutes);
-router.use('/api', apiRoutes);
-export default router;
